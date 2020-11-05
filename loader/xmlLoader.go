c33449f2fec07d16012783f1abf29dd4a7d09890
luo2pei4 <luo2pei4321@outlook.com>
2020-11-05 21:46:27 +0900

    优化代码，将栈存储节点名称改为存储节点的指针，这样就可以不用通过名字再来找父节点，而是直接通过栈就可以获取父节点。

--- loader/xmlLoader.go
+++ loader/xmlLoader.go
@@ -8,68 +8,6 @@ import (
 	"strings"
 )
 
-// stack 深度为10的栈，用来压入XML的标签名称。
-type stack struct {
-	MaxTop int      // 栈顶最大值
-	Top    int      // 栈顶标识
-	arr    []string // 数组
-}
-
-func (s *stack) push(name string) error {
-
-	if s.Top == s.MaxTop-1 {
-		return errors.New("push(), stack full")
-	}
-
-	s.Top++
-	s.arr[s.Top] = name
-
-	return nil
-}
-
-func (s *stack) pop() (name string, err error) {
-
-	if s.Top == -1 {
-		return "", errors.New("pop(), stack empty")
-	}
-
-	name = s.arr[s.Top]
-	s.Top--
-
-	return name, nil
-}
-
-func initStack(MaxTop, Top int) *stack {
-
-	// 创建栈对象，最多支持16层嵌套
-	return &stack{
-		MaxTop: 16,
-		Top:    -1,
-		arr:    make([]string, 16),
-	}
-}
-
-func (s *stack) topValue() (name string, err error) {
-
-	if s.Top == -1 {
-		return "", errors.New("topValue(),stack empty")
-	}
-
-	name = s.arr[s.Top]
-
-	return name, nil
-}
-
-// previousValue 获取数组倒数第二个值
-func (s *stack) previousValue() (name string, level int, err error) {
-
-	if s.Top-1 == -1 {
-		return "", 0, nil
-	}
-
-	return s.arr[s.Top-1], s.Top - 1, nil
-}
-
 // Node XML的节点结构体
 type Node struct {
 	name       string            // 标签名称
@@ -95,7 +33,14 @@ func (node *Node) PrintNode() {
 		}
 	}
 
-	fmt.Println(node.name, node.value, attrbutes)
+	if node.parentNode == nil {
+
+		fmt.Println(node.name, node.value, attrbutes)
+
+	} else {
+
+		fmt.Println(node.parentNode.name, node.name, node.value, attrbutes)
+	}
 
 	childNodes := node.childNodes
 
@@ -108,33 +53,61 @@ func (node *Node) PrintNode() {
 	}
 }
 
-// 获取当前节点的父节点
-func getParentNode(stack *stack, node *Node, previousName string, previousLevel int) *Node {
+// stack 深度为10的栈，用来压入XML的标签名称。
+type stack struct {
+	MaxTop int     // 栈顶最大值
+	Top    int     // 栈顶标识
+	arr    []*Node // 数组
+}
 
-	if node.name != previousName {
+func (s *stack) push(node *Node) error {
 
-		for _, childNode := range node.childNodes {
-
-			node = getParentNode(stack, childNode, previousName, previousLevel)
-		}
+	if s.Top == s.MaxTop-1 {
+		return errors.New("push(), stack full")
 	}
 
-	return node
+	s.Top++
+	s.arr[s.Top] = node
+
+	return nil
+}
+
+func (s *stack) pop() (node *Node, err error) {
+
+	if s.Top == -1 {
+		return nil, errors.New("pop(), stack empty")
+	}
+
+	node = s.arr[s.Top]
+	s.Top--
+
+	return node, nil
+}
+
+func (s *stack) topValue() *Node {
+
+	return s.arr[s.Top]
+}
+
+func initStack(MaxTop, Top int) *stack {
+
+	// 创建栈对象，最多支持16层嵌套
+	return &stack{
+		MaxTop: 16,
+		Top:    -1,
+		arr:    make([]*Node, 16),
+	}
 }
 
 // LoadXML 通过文件路径加载xml，并返回解析的节点链
-func LoadXML(xmlDec *xml.Decoder) *Node {
+func LoadXML(xmlDec *xml.Decoder) (root *Node, err error) {
 
 	// 初始化栈
 	s := initStack(10, -1)
 
 	// 创建节点链
-	root := new(Node)
-
-	// 声明父节点和当前节点
-	// 声明父节点用于保存父节点，减少递归调用寻找父节点的次数
-	var parentNode *Node
-	var currentNode *Node
+	root = new(Node)
+	var node *Node
 
 	var startElementLoaded bool = false
 
@@ -150,7 +123,7 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 		}
 
 		if err != nil {
-			panic(err)
+			return nil, errors.New("loader.LoadXML, xmlDec.Token() error")
 		}
 
 		// 判断节点类型
@@ -160,7 +133,6 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 		case xml.StartElement:
 
 			name := t.Name.Local
-			s.push(name)
 			var attr map[string]string
 
 			if len(t.Attr) != 0 {
@@ -172,34 +144,32 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 				}
 			}
 
-			if s.Top == 0 {
+			if s.Top == -1 {
 
 				root.name = name
 				root.attr = attr
 				root.level = s.Top
 				root.childNodes = make([]*Node, 0)
-				parentNode = root
+
+				s.push(root)
 
 			} else {
 
-				currentNode = &Node{
+				node = &Node{
 					name:       name,
 					attr:       attr,
 					level:      s.Top,
 					childNodes: make([]*Node, 0),
 				}
 
-				previousName, previousLevel, _ := s.previousValue()
+				previousNode := s.topValue()
 
-				if parentNode.name != previousName {
-					parentNode = getParentNode(s, parentNode, previousName, previousLevel)
-				}
-
-				currentNode.parentNode = parentNode
-				parentNode.childNodes = append(parentNode.childNodes, currentNode)
+				node.parentNode = previousNode
+				previousNode.childNodes = append(previousNode.childNodes, node)
 
 				// 只有在非根节点的开始元素载入时才会设置为true
 				startElementLoaded = true
+				s.push(node)
 			}
 
 		// 将值存入节点链中最后一个节点
@@ -207,7 +177,7 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 
 			// 根节点后面的换行符会被识别为xml.CharData类型，currentNode还没有被实例化，
 			// 此时向currentNode写入值会产生panic，所以在此处要做一个非nil的判断
-			if currentNode != nil {
+			if node != nil {
 
 				// 判断是否已经载入了开始元素
 				if startElementLoaded {
@@ -215,24 +185,15 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 					// 处理开始元素后面的换行符和第二行的空格
 					value := string(t)
 					value = strings.Replace(value, "\n", "", -1)
-					currentNode.value = value
+					node.value = value
 				}
 			}
 
 		// 结束标签，主要将当前标签名弹出栈
 		case xml.EndElement:
-			end := t.Name.Local
-			top, err := s.topValue()
-
-			if err != nil {
-				fmt.Println("xml.EndElement ", err.Error())
-			}
-
-			if end == top {
-				s.pop()
-			}
 
 			startElementLoaded = false
+			s.pop()
 
 		// Comment，ProcInst和Directive类型的内容不做处理
 		case xml.Comment:
@@ -243,5 +204,5 @@ func LoadXML(xmlDec *xml.Decoder) *Node {
 		}
 	}
 
-	return root
+	return
 }
